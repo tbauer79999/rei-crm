51f18c0046a2d1398a53a33cbea65e68282d5f13
Tom Bauer <you@example.comthomasbauer1008@gmail.com>
2025-07-17 22:46:33 -0400

    cors1

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
-
+// ADD THIS LINE RIGHT HERE:
+app.options('*', cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
