a5d64cd6aa79a493834f0d571b6e9e7eeed6c8f0
Tom Bauer <you@example.comthomasbauer1008@gmail.com>
2025-08-01 19:17:51 -0400

    1

--- server.js
+++ server.js
@@ -53,8 +53,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
-console.log("helo")
-
+console.log("hello")
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/hot-summary', hotSummaryRoutes);
